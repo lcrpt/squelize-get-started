0d26f1921798644ea17f0d17656d738beb8bc7fd
leolassence <leolassence@gmail.com>
2016-11-22 13:38:23 +0100

    added models schema with sequelize

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const connection = new Sequelize('sequelize_get_started', 'root', 'root', {
 });
 
 const Article = connection.define('article', {
+  slug: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+  },
   title: {
     type: Sequelize.STRING,
     unique: true,
@@ -13,24 +17,26 @@ const Article = connection.define('article', {
   },
   body: {
     type: Sequelize.TEXT,
-    defaultValue: 'no body set',
   },
 }, {
-  timestamp: false,
+  timestamps: false,
 });
 
-connection.sync().then(() => {
-  Article.findAll().then((articles) => {
-    console.log(articles.length);
-  })
+connection.sync({
+  force: true,
+}).then(() => {
+  Article.create({
+    slug: "kickStation-first-podcast",
+    title: "KickStation first podcast",
+    body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+  }).then(() => {
+    Article.findAll().then((articles) => {
+      console.log('findAll length -> ', articles.length);
+    });
+    Article.findById('kickStation-first-podcast').then((article) => {
+      console.log('findById -> ', article.dataValues);
+    });
+  });
 }).catch((err) => {
   console.error(err);
 });
-
-// Article.create({
-//   title: "KickStation first podcast",
-//   body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-// });
-// Article.findById(1).then((article) => {
-//   console.log(article.dataValues);
-// })
